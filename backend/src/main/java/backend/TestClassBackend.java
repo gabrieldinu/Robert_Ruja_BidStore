6fe6071b888b8f29dd065682031f2bf810b720c8
robert.ruja <rja@gmail.com>
2017-04-10 12:50:20 +0300

    test git2

---
+++ backend/src/main/java/backend/TestClassBackend.java
@@ -0,0 +1,11 @@
+package backend;
+
+/**
+ * Created by robert.ruja on 07-Apr-17.
+ */
+public class TestClassBackend {
+
+    public void callBackend(){
+        System.out.println("hello from backend");
+    }
+}
